3f0bb5ec961dcd3477b024749f832c11458896f4
raymond gitonga <raytosh95@gmail.com>
2020-03-29 17:39:10 +0300

    Initial commit

---
+++ src/main/java/com/tosh/ZuulProxy/ZuulProxyApplication.java
@@ -0,0 +1,13 @@
+package com.tosh.ZuulProxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulProxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyApplication.class, args);
+	}
+
+}
