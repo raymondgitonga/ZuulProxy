2e309bacc93d77956acc3314cbb2f3291dd79339
raymond gitonga <raytosh95@gmail.com>
2020-03-30 02:16:11 +0300

    Add custom filter

--- src/main/java/com/tosh/ZuulProxy/ZuulProxyApplication.java
+++ src/main/java/com/tosh/ZuulProxy/ZuulProxyApplication.java
@@ -1,9 +1,11 @@
 package com.tosh.ZuulProxy;
 
+import com.tosh.ZuulProxy.filter.CustomFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
@@ -12,6 +14,11 @@ public class ZuulProxyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulProxyApplication.class, args);
+	}
+
+	@Bean
+	public CustomFilter getFilter(){
+		return new CustomFilter();
 	}
 
 }
---
+++ src/main/java/com/tosh/ZuulProxy/filter/CustomFilter.java
@@ -0,0 +1,38 @@
+package com.tosh.ZuulProxy.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+
+public class CustomFilter extends ZuulFilter {
+
+    private static Logger logger = LoggerFactory.getLogger(CustomFilter.class);
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext context = RequestContext.getCurrentContext();
+        HttpServletRequest request = context.getRequest();
+        logger.info(String.format("%s request %s", request.getMethod(), request.getRequestURL()));
+        return null;
+    }
+}
